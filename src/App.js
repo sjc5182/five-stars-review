616d9fb2b67c20b40b0be0905ca7fe70d09fa480
sjc5182 <shiguangchen1987@gmail.com>
2019-04-08 18:24:56 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './app.css'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      isToggleOn: [true, true, true, true, true]
+    }
+  }
+
+  changeColor = (e) => {
+    let starArray = this.state.isToggleOn
+    for(let i = 0; i<=e.target.id; i++){
+      starArray[i]=false
+    }
+    this.setState({
+      isToggleOn: starArray
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className = "Container">
+        <div className = 'Wrapper'>
+          <div className = 'innerWraper'>
+            <i id = "0" className="far fa-star" style = {{fontSize: '250%', color: this.state.isToggleOn[0] ? 'green' : 'red' }} onClick= {this.changeColor}></i>
+            <i id = "1" className="far fa-star" style = {{fontSize: '250%', color: this.state.isToggleOn[1] ? 'green' : 'red' }} onClick= {this.changeColor}></i>
+            <i id = "2" className="far fa-star" style = {{fontSize: '250%', color: this.state.isToggleOn[2] ? 'green' : 'red' }} onClick={this.changeColor}></i>
+            <i id = "3" className="far fa-star" style = {{fontSize: '250%', color: this.state.isToggleOn[3] ? 'green' : 'red' }} onClick={this.changeColor}></i>
+            <i id = "4" className="far fa-star" style = {{fontSize: '250%', color: this.state.isToggleOn[4] ? 'green' : 'red' }} onClick={this.changeColor}></i>
+          </div>
+        </div>
       </div>
     );
   }

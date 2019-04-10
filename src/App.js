c8749483009f99cc295e7b40c65255cb254e77ee
sjc5182 <shiguangchen1987@gmail.com>
2019-04-09 21:43:50 -0700

    update font size

--- src/App.js
+++ src/App.js
@@ -24,11 +24,11 @@ class App extends Component {
       <div className = "Container">
         <div className = 'Wrapper'>
           <div className = 'innerWraper'>
-            <i id = "0" className="far fa-star" style = {{fontSize: '250%', color: this.state.isToggleOn[0] ? 'green' : 'red' }} onClick= {this.changeColor}></i>
-            <i id = "1" className="far fa-star" style = {{fontSize: '250%', color: this.state.isToggleOn[1] ? 'green' : 'red' }} onClick= {this.changeColor}></i>
-            <i id = "2" className="far fa-star" style = {{fontSize: '250%', color: this.state.isToggleOn[2] ? 'green' : 'red' }} onClick={this.changeColor}></i>
-            <i id = "3" className="far fa-star" style = {{fontSize: '250%', color: this.state.isToggleOn[3] ? 'green' : 'red' }} onClick={this.changeColor}></i>
-            <i id = "4" className="far fa-star" style = {{fontSize: '250%', color: this.state.isToggleOn[4] ? 'green' : 'red' }} onClick={this.changeColor}></i>
+            <i id = "0" className="far fa-star" style = {{fontSize: '300%', color: this.state.isToggleOn[0] ? 'green' : 'red' }} onClick= {this.changeColor}></i>
+            <i id = "1" className="far fa-star" style = {{fontSize: '300%', color: this.state.isToggleOn[1] ? 'green' : 'red' }} onClick= {this.changeColor}></i>
+            <i id = "2" className="far fa-star" style = {{fontSize: '300%', color: this.state.isToggleOn[2] ? 'green' : 'red' }} onClick={this.changeColor}></i>
+            <i id = "3" className="far fa-star" style = {{fontSize: '300%', color: this.state.isToggleOn[3] ? 'green' : 'red' }} onClick={this.changeColor}></i>
+            <i id = "4" className="far fa-star" style = {{fontSize: '300%', color: this.state.isToggleOn[4] ? 'green' : 'red' }} onClick={this.changeColor}></i>
           </div>
         </div>
       </div>
